fdc1447cbd14985ef964f7475ddbf686e6715211
Teresa <teresa.wu@ostmodern>
2018-05-21 16:29:23 +0100

    tidy up result string

--- tensorflow/src/main/java/app/co/uk/tensorflow/model/Result.kt
+++ tensorflow/src/main/java/app/co/uk/tensorflow/model/Result.kt
@@ -5,10 +5,8 @@ import android.graphics.RectF
 class Result(val id: String?, val title: String?, val confidence: Float?, private var location: RectF?) {
     override fun toString(): String {
         var resultString = ""
-        if (id != null) resultString += "[$id] "
-        if (title != null) resultString += title + " "
-        if (confidence != null) resultString += String.format("(%.1f%%) ", confidence * 100.0f)
-        if (location != null) resultString += location!!.toString() + " "
-        return resultString.trim { it <= ' ' }
+        if (title != null) resultString += title + ": "
+        if (confidence != null) resultString += confidence.toString() 
+        return resultString
     }
 }
