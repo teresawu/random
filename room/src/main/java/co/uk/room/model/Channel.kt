887544494a1c6816ff393d3b15500ed8f848d045
Teresa <teresa.wu@ostmodern>
2018-06-07 14:04:50 +0100

    make changes in gradle

--- room/src/main/java/co/uk/room/model/Channel.kt
+++ room/src/main/java/co/uk/room/model/Channel.kt
@@ -1,4 +1,16 @@
 package co.uk.room.model
 
-data class Channel(var name: String = "", var kind: String = "", var etag: String = "", var nextPageToken: String = "",
-                   var pageInfo: PageInfo? = null, var items: List<Item> = ArrayList())
+import android.arch.persistence.room.Entity
+import java.util.*
+
+@Entity
+data class Channel(
+        @PrimaryKey(autoGenerate = false)
+        val primaryKey: String = UUID.randomUUID().toString(),
+        var name: String = "",
+        var kind: String = "",
+        var etag: String = "",
+        var nextPageToken: String = "",
+        var pageInfo: PageInfo? = null,
+        var items: List<Item> = ArrayList()
+)
